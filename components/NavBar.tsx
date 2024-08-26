12e16e4516b2e7b8e20c5c6f58d4f1fa2b7ac12c
iJoyRide <sumfeis3@gmail.com>
2024-08-26 16:52:55 +0800

    NavBar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -9,14 +9,15 @@ const borderClass = "border border-red-500"
 const NavBar: React.FC = () => {
     const router = useRouter();
     return (
-        <nav className={`grid grid-cols-5 text-center p-5 text-sm`}>
-            <div>Logo</div>
-            <div className={`col-span-4 flex justify-evenly items-center ${borderClass}`}>
+        <div className="flex justify-center p-5 sticky">
+            <nav className="flex justify-evenly w-1/2">
+                <>logo</>
                 <button className={hoverClass} onClick={() => router.push('/about')} aria-label="About Page">About</button>
                 <button className={hoverClass} onClick={() => router.push('/classes')} aria-label="Classes Page">Classes</button>
                 <button className={hoverClass} onClick={() => router.push('/contact')} aria-label="Contact Us Page">Contact Us</button>
-            </div>
-        </nav>
+            </nav>
+        </div>
+        
     );
 }
 
